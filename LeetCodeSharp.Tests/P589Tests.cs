c62e7a9706ef666128a6fc484512fbe632f61ea3
jackfiled <xcrenchangjun@outlook.com>
2024-02-18 14:47:25 +0800

    add: 单元测试项目

---
+++ LeetCodeSharp.Tests/P589Tests.cs
@@ -0,0 +1,22 @@
+﻿using LeetCodeSharp.Problems;
+using LeetCodeSharp.Utils;
+
+namespace LeetCodeSharp.Tests;
+
+public class P589Tests
+{
+    [Fact]
+    public void Test1()
+    {
+        Node root = new Node(1, [
+            new Node(3),
+            new Node(2),
+            new Node(4),
+            ]);
+
+        IList<int> except = [1, 3, 2, 4];
+
+        var solution = new Solution();
+        Assert.Equal(except, solution.Preorder(root));
+    }
+}
--- LeetCodeSharp/Utils/Node.cs
+++ LeetCodeSharp/Utils/Node.cs
@@ -12,6 +12,7 @@ namespace LeetCodeSharp.Utils
         public Node(int _val)
         {
             val = _val;
+            children = new List<Node>();
         }
 
         public Node(int _val, IList<Node> _children)
